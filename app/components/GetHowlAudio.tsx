9a372870d0e6ef064b64399dd7a2281eddf60a9b
KimDongWoo <t6u80o@naver.com>
2024-06-04 17:58:07 +0900

    f: 볼륨 조절 input 설정

--- app/components/GetHowlAudio.tsx
+++ app/components/GetHowlAudio.tsx
@@ -7,33 +7,51 @@ export function GetHowlAudio (audio :any){
     audio = audio.audio
 
     let [howlAudio, setHowlAudio] = useState<Howl | null>(null);
+    let [volume, setVolume] = useState(0.5)
 
     useEffect(()=>{
-        const sound :any = new Howl({
+        const sound :Howl = new Howl({
             src: [audio.audioUrl],
             format: ['mp3'],
+            volume: 0.5
         });
 
         setHowlAudio(sound)
     },[])
 
+    useEffect(() => {
+        if(howlAudio){
+            if (volume === 1) {
+                howlAudio.mute();
+            } else {
+                howlAudio.volume(volume);
+            }
+        }
+      }, [volume]);
+
     return(
         <div>
             <button onClick={()=>playAudio(howlAudio)}>재생</button>
             <button onClick={()=>pauseAudio(howlAudio)}>중지</button>
+            <div>
+                <input type='range' min="1" max="100" defaultValue="50" onChange={(e)=>{
+                    let nowVolume :number = e.target.value /100;
+                    setVolume((nowVolume)/100)
+                }}/>
+            </div>
         </div>
     )
 }
 
 // howl 객체 조절 함수1 (플레이)
-export const playAudio = (howlAudio :any) => {
+export const playAudio = (howlAudio :Howl | null) => {
     if (howlAudio) {
         howlAudio.play();
     }
 };
 
 // howl 객체 조절 함수2 (종료)
-export const pauseAudio = (howlAudio :any) => {
+export const pauseAudio = (howlAudio :Howl | null) => {
     if (howlAudio) {
         howlAudio.pause();
     }
