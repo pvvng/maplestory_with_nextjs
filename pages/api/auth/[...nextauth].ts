ad28d1834d29327a225bc2f5720cf81dc25633d9
김동우 <t6u80o@naver.com>
2024-06-10 01:23:00 +0900

    f: callback url 설정

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,9 +13,15 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
     }),
   ],
-  secret : process.env.SOCIAL_LOGIN_SECRET
+  secret : process.env.SOCIAL_LOGIN_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl } :{url:any, baseUrl:any}) {
+      // Ensure the URL is relative to the base URL
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Otherwise, ensure it is the same origin URL
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    }
+  }
 };
 export default NextAuth(authOptions); 
-
-// https://whkakrkr.tistory.com/entry/Next-auth%EB%A1%9C-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-5%EB%B6%84%EB%A7%8C%EC%97%90-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-%F0%9F%91%BB
-// https://velog.io/@uni/NextAuth.js-%EA%B5%AC%EA%B8%80-%EB%A1%9C%EA%B7%B8%EC%9D%B8-Next%EB%B2%84%EC%A0%84-13.4.2
